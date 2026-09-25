b13677a969342238fda8823e3b3457be07331212
Steve Taylor <ts19700620@gmail.com>
2026-09-25 11:25:13 -0400

    new flask app

---
+++ chatbot/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS		# newly added
+
+app = Flask(__name__)
+CORS(app)				# newly added
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+
+if __name__ == '__main__':
+    app.run()
